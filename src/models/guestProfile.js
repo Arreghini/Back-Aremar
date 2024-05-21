7dd675743f7b4c6b64b744b9625890de37763153
Arreghini <marreghi@gmail.com>
2024-05-21 18:11:46 -0300

    models 21/5

--- src/models/guestProfile.js
+++ src/models/guestProfile.js
@@ -1,45 +1,55 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-
   sequelize.define('guestProfile', {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4, 
       allowNull: false, 
       primaryKey: true, 
-        },
+    },
     firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-               }
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
     password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [8, 20],
-                isAlphanumeric: true,
-                }
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 20],
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },    
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
     phone: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isNumeric: true,
-                len: [10, 10],
-            }
-        },   
-    }   
-  )};
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        len: [10, 10],
+      }
+    },
+  });
+};
---
+++ src/models/reservations.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('reservations', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    roomId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    guests: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    checkInDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      }
+    },
+    checkOutDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      }
+    },
+    totalPrice: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('pending', 'confirmed', 'cancelled'),
+      defaultValue: 'pending',
+    },
+  });
+};
