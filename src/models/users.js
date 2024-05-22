b3b15f6ca02b808a14d355dbc309a5d379db6f6a
Arreghini <marreghi@gmail.com>
2024-05-22 15:51:36 -0300

    models 22/5

---
+++ src/models/users.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('users', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4, 
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },    
+    email: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+      },
+    emailValidate: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        default: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+});
+}
