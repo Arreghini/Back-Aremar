a207125465b1509dc0dae6a88daa0b1cee6f7154
Arreghini <marreghi@gmail.com>
2024-08-27 14:06:37 -0300

    Aplicar .gitignore

--- src/handlers/room/getRoomByQueryHandler.js
+++
@@ -1,7 +0,0 @@
-const getRoomByQuery = async (req, res) => {
-    // Lógica para obtener habitaciones por consulta
-    res.send('Rooms by query');
-  };
-  
-  module.exports = getRoomByQuery;
-  
--- src/routes/RoomTypeRoutes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function RoomTypeRoutes() {
-  return (
-    <div>RoomTypeRoutes</div>
-  )
-}
-
-export default RoomTypeRoutes
