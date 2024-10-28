940058348ed1f963a34bcd1f569169b2cea37c5a
Mauricio Arreghini <marreghi@gmail.com>
2024-10-28 00:02:45 -0300

    actualizar

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.name = 'API';
 
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:4000'],
-  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
