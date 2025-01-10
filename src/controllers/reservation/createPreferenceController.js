2c154b83fdb2cc9fa4439abdb84f358f3a196f5d
Mauricio Arreghini <marreghi@gmail.com>
2025-01-10 00:05:26 -0300

    implementando mercadopago

--- src/controllers/reservation/createPreferenceController.js
+++ src/controllers/reservation/createPreferenceController.js
@@ -1,25 +1,32 @@
-const mercadopago = require('mercadopago');
-mercadopago.configure({
-    access_token: process.env.MERCADOPAGO_ACCESS_TOKEN
+const { MercadoPagoConfig, Preference } = require('mercadopago');
+
+const client = new MercadoPagoConfig({ 
+    accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN 
 });
 
 const createPreferenceController = async (reservationId, amount, currency) => {
-    const preference = {
-      items: [{
-        title: `Reserva #${reservationId}`,
-        unit_price: amount,
-        quantity: 1,
-        currency_id: currency
-      }],
-      back_urls: {
-        success: `${process.env.FRONTEND_URL}/payment/success`,
-        failure: `${process.env.FRONTEND_URL}/payment/failure`
-      },
-      auto_return: "approved"
-    };
-  
-    return await mercadopago.preferences.create(preference);
-  };
-  
-  module.exports = createPreferenceController;
-  
+    try {
+        const preference = new Preference(client);
+        const preferenceData = {
+            items: [{
+                title: `Reserva #${reservationId}`,
+                unit_price: Number(amount),
+                quantity: 1,
+                currency_id: currency
+            }],
+            back_urls: {
+                success: `${process.env.FRONTEND_URL}/payment/success`,
+                failure: `${process.env.FRONTEND_URL}/payment/failure`
+            },
+            auto_return: "approved"
+        };
+        
+        const result = await preference.create({ body: preferenceData });
+        return result;
+    } catch (error) {
+        console.log("Error en MercadoPago:", error);
+        throw error;
+    }
+};
+
+module.exports = createPreferenceController;
